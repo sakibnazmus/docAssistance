362ccd8db5166fce2ff828eb698d3c3b6781f78d
sakibnazmus <nazmus.sakib.czs2012@gmail.com>
2020-02-04 02:53:31 +0600

    Added sign up page

--- app/src/main/java/com/example/docassistance/MainActivity.java
+++ app/src/main/java/com/example/docassistance/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.docassistance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signupBtn = findViewById(R.id.signUpBtn);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
