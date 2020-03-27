59f164dd5063070e41b5faadc34e8cbcb86b1b0a
Mahmudurrahmanpromise <promiserahman38@gmail.com>
2020-03-27 10:00:53 +0600

    UI updated

---
+++ app/src/main/java/com/example/docassistance/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.docassistance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signUp = findViewById(R.id.text_sign_up);
+
+        signUp.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+    }
+
+}
